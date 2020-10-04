373357d5cfd66dab5c0daafec135e113d1e198a6
tsplitter <pa.tsplitter@yahoo.com>
2020-10-04 06:01:24 +0100

    Completed module

--- budget/Expense.py
+++ budget/Expense.py
@@ -49,7 +49,6 @@ class Expenses():
          
         food_expenses = {x for x in self.list 	
                             if x.category == 'Groceries' or x.category == 'Eating Out'}
-     
-        unnecesary_expenses = set(self.list) - necessary_expenses - food_expenses
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
         return [necessary_expenses, food_expenses, unnecessary_expenses]
 
