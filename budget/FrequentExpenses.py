df1434abf08990c6f338a5d1861be43487316f30
tsplitter <pa.tsplitter@yahoo.com>
2020-10-04 03:08:31 +0100

    Completed module

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -7,7 +7,7 @@ expenses.read_expenses('data/spending_data.csv')
 
 spending_categories = []
 
-for expense in expense.list:
+for expense in expenses.list:
     spending_categories.append(expense.category)
 
 spending_counter = collections.Counter(spending_categories)
